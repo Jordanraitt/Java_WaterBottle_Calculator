e1421669a7d9023e4b27eebec8ac46d2eb9ce48c
jordanraitt <jordanraitt@hotmail.com>
2018-11-19 16:42:39 +0000

    clac

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,36 @@
 public class Calculator {
+
+
+    public Calculator(){
+    }
+
+    public int calcAdd(int number1, int number2){
+        int result = (number1 + number2);
+        {
+            return result;
+        }
+    }
+
+    public int calcSub(int number1, int number2){
+        int result = (number1 - number2);
+        {
+            return result;
+        }
+    }
+
+    public int calcMulti(int number1, int number2){
+        int result = (number1 * number2);
+        {
+            return result;
+        }
+    }
+
+    public int calcDiv(int number1, int number2){
+        int result = (number1 / number2);
+        {
+            return result;
+        }
+    }
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,16 +1,38 @@
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTest {
+import static org.junit.Assert.assertEquals;
 
-    @Test
+
+public class CalculatorTest {
 
     private Calculator calculator;
 
     @Before
+    public void before() {
+        calculator = new Calculator();
+    }
 
+    @Test
+    public void calcAdd(){
+//        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.calcAdd(1,1));
+    }
 
+    @Test
+    public void clacSub(){
+//        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.calcSub(1,1));
+    }
 
+    @Test
+    public void calcMulti(){
+        assertEquals(4,calculator.calcMulti(2,2));
+    }
 
+    @Test
+    public void calcDiv(){
+        assertEquals(2, calculator.calcDiv(4,2));
+    }
 
 }
